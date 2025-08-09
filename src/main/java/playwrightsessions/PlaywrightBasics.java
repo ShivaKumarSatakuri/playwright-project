f588c0dc5ccd49bad8e87f520fd5e27e784b49f8
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-08-09 23:20:24 +0530

    Added PlaywrightBasics class  - Added basic playwright functionality

---
+++ src/main/java/playwrightsessions/PlaywrightBasics.java
@@ -0,0 +1,31 @@
+package playwrightsessions;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlaywrightBasics {
+
+    public static void main(String... args) {
+
+        Playwright playwright = Playwright.create();
+
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+        page.navigate("https://www.google.com");
+
+        System.out.println("Playwright started successfully");
+
+        String title = page.title();
+        System.out.println("Page title is : " + title);
+
+        String url = page.url();
+        System.out.println("Page url is : " + url);
+
+        browser.close();
+        System.out.println("Browser closed successfully");
+
+        playwright.close();
+        System.out.println("Playwright closed successfully");
+    }
+}
