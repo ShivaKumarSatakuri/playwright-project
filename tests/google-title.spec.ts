859b876b1412d8381f3e700755b3e1cf45f58c86
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-08-14 22:09:25 +0530

    google-title.spec.ts  - Added comments

--- tests/google-title.spec.ts
+++ tests/google-title.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test'
 
+
+// Writing a very basic test case
 test('Google homepage title', async ({ page }) => {
+
+    //Wait until the page is loaded
     await page.goto('https://google.com')
 
+    //Wait until the title is loaded. Then verify if title cotains the text 'Google'
     await expect(page).toHaveTitle(/Google/)
 })
