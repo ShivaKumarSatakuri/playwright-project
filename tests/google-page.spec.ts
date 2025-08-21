d94a02ac71bab97c6045db86f28c09cf0d730300
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-08-21 20:23:42 +0530

    google-page.spec.ts  - Fixed locator issue

--- tests/google-page.spec.ts
+++ tests/google-page.spec.ts
@@ -6,16 +6,13 @@ test('Google page test', async () => {
     const page: Page = await browser.newPage();
     await page.goto('https://www.google.com/')
 
-    //TODO:  Need to work on locators
-    //const inputTextBox : Locator = await page.locator('xpath=/html/body/div[2]/div[4]/form/div[1]/div[1]/div[1]/div[1]/div[2]/textarea')
-    const inputTextBox : Locator = await page.locator('xpath=/input[@title="Search"]');
-    //const searchButton : Locator = await page.locator('xpath=/html/body/div[2]/div[4]/form/div[1]/div[1]/div[3]/center/input[1]')
 
-    inputTextBox.fill("Shiva Kumar Satakuri LinkiedIn");
+    const inputTextBox : Locator = await page.locator('//textarea[@name="q"]')
+    inputTextBox.fill('Shiva Kumar Satakuri LinkiedIn');
     inputTextBox.press('Enter');
     //searchButton.click();
 
-    await page.waitForTimeout(10000)
+    await page.waitForTimeout(20000)
     page.close();
     browser.close();
 })
