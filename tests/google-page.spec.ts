c21f71427c1d2d19fc2345cb4443ead2faa59f8b
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-08-22 22:26:45 +0530

    google-page.spec.ts  - testing hyperlink click

--- tests/google-page.spec.ts
+++ tests/google-page.spec.ts
@@ -6,16 +6,16 @@ test('Google page test', async () => {
     const page: Page = await browser.newPage();
     await page.goto('https://www.google.com/')
 
-    //TODO:  Need to work on locators
-    //const inputTextBox : Locator = await page.locator('xpath=/html/body/div[2]/div[4]/form/div[1]/div[1]/div[1]/div[1]/div[2]/textarea')
-    const inputTextBox : Locator = await page.locator('xpath=/input[@title="Search"]');
-    //const searchButton : Locator = await page.locator('xpath=/html/body/div[2]/div[4]/form/div[1]/div[1]/div[3]/center/input[1]')
 
-    inputTextBox.fill("Shiva Kumar Satakuri LinkiedIn");
+    const inputTextBox : Locator = await page.locator('//textarea[@name="q"]')
+    inputTextBox.fill('Shiva Kumar Satakuri LinkiedIn');
     inputTextBox.press('Enter');
+
     //searchButton.click();
 
-    await page.waitForTimeout(10000)
+    await page.waitForTimeout(20000)
+
+    page.locator('xpath=/html/body/div[3]/div/div[12]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div[1]/div/div/span/a/h3').click
     page.close();
     browser.close();
 })
