cd1cf40ebf65f57906b670d3b051f5c31005c7dd
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-09-07 17:36:01 +0530

    testing let var and const

---
+++ typescript-practice/src/TSBasics.ts
@@ -0,0 +1,17 @@
+export class TSBasics {
+
+    readonly pi = 3.14;
+    readonly values = [1,2,3]
+
+    TSBasics() {
+        var x = 10;
+        console.log(x)
+        this.testMethod()
+    }
+
+
+    testMethod() {
+        let xVal = 10;
+        console.log(xVal)
+    }
+}
